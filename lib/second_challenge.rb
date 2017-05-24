04685f425f8a7fccea0563523b43a353fe66305f
davidtom <davidjtomczyk@gmail.com>
2017-05-24 13:12:52 -0400

    completed methods

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def second_challenge
   groceries = {
    dairy: ["milk", "yogurt", "cheese"],
@@ -7,7 +9,11 @@ def second_challenge
   }
 
   #code your solution here!
-
-  
-
+  flat_array = []
+  groceries.values.each {|array|
+    array.each { |item|
+      flat_array << item
+    }
+  }
+  flat_array
 end
